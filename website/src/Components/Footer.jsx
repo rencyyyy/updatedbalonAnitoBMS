b367468a650120261b0c7186f54e1e3dcae3c1f7
capt1nedev <justineobasibas@iskolarngbayan.pup.edu.ph>
2024-08-24 16:48:08 +0800

    Edit footer react icon, edit json file

--- website/src/Components/Footer.jsx
+++ website/src/Components/Footer.jsx
@@ -9,12 +9,12 @@ export default function Footer() {
     <footer className=" text-black py-6 bg-green-100">
       <div className="container mx-auto w-full px-2">
         <div>
-          
+
           <div className="flex justify-center gap-[5%] mt-3 flex-wrap basis-52 grow">
-            
+
             <div className="w-52 text-center">
               <p className="mb-3 mt-5 font-bold text-xl text-green-500">Quick Links</p>
-              
+
               <ul className="text-gray-600">
                 <li><HashLink to={"/#about"}>About</HashLink></li>
                 <li><HashLink to={"/#services"}>Services</HashLink></li>
@@ -25,7 +25,7 @@ export default function Footer() {
 
             <div className="w-52 text-center">
               <p className="mb-3 mt-5 font-bold text-xl text-green-500">Our Community</p>
-              
+
               <ul className="text-gray-600">
                 <li>
                   <Link to={"https://www.google.com/maps/place/Balon+Anito+Barangay+Hall,+National+Rd,+Mariveles,+Bataan/@14.4363712,120.4696933,17z/data=!3m1!4b1!4m6!3m5!1s0x3396192d3c682871:0x801a23e613c67725!8m2!3d14.4363745!4d120.4722831!16s%2Fg%2F11bvt9xf0y?authuser=0&entry=ttu"}>
@@ -38,7 +38,7 @@ export default function Footer() {
 
             <div className="w-52 text-center">
               <p className="mb-3 mt-5 font-bold text-xl text-green-500">Contact Us</p>
-              
+
               <ul className="text-gray-600">
                 <li>No: 09101010101</li>
                 <li><Link to={"/"}>Email Address: brgybalonanito@gmail.com</Link></li>
@@ -47,7 +47,7 @@ export default function Footer() {
 
             <div className="w-52 text-center">
               <p className="mb-3 mt-5 font-bold text-xl text-green-500">Social Media Links</p>
-              
+
               <ul className="text-2xl flex justify-evenly">
                 <li><Link to={"https://www.facebook.com/kabataanngBalonAnito"}><FaFacebookSquare /></Link></li>
                 <li><Link to={"/"}><SiGmail /></Link></li>
@@ -57,12 +57,12 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        
+
       </div>
 
       <div className="text-center text-lg flex justify-center items-center py-3 w-full mt-10">
-          <p><strong>&copy;</strong>2024 <strong>Balon Anito</strong> | All rights reserved.</p>
-        </div>
+        <p><strong>&copy;</strong>2024 <strong>Balon Anito</strong> | All rights reserved.</p>
+      </div>
     </footer>
   )
 }

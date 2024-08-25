144a49b0ccb5e620e604a9d7aef12eb989b394e4
JustineSantos <justine0521>
2024-08-25 23:18:09 +0800

    Edit Navbar, Edit App.jsx, Add About, Add Chat bot, Edit Home.jsx, Add FAQ, Add About Images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
-import Home from './Pages/Home';
 import Footer from './Components/Footer';
 import RequestForm from './Components/RequestForm';
 import { ActiveSectionProvider } from './Components/ActiveSectionContext';
@@ -17,6 +16,14 @@ import CertificateOfGoodMoral from './Request-Certificate/CertificateOfGoodMoral
 import CertificateForNewResidents from './Request-Certificate/CertificateForNewResidents'
 import CertificateForSoloParent from './Request-Certificate/CertificateForSoloParent'
 
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Services from './Pages/Services';
+import TrackRequest from './Pages/TrackRequest';
+import Faq from './Pages/Faq';
+import Location from './Pages/Location';
+
+
 function App() {
   return (
     <main>
@@ -27,15 +34,21 @@ function App() {
             <Route path="/" element={<Home />} />
 
             <Route path="/barangay-clearance-form" element={<BarangayClearance />} />
-            <Route path='/certificate-of-residency-form' element={<CertificateOfResidency />} />
-            <Route path='/certificate-of-indigency-form' element={<CertificateOfIndigency />} />
-            <Route path='/certificate-of-low-income-form' element={<CertificateOfLowIncome />} />
-            <Route path='/business-clearance-form' element={<BusinessClearance />} />
-            <Route path='/certificate-of-death-form' element={<CertificateOfDeath />} />
-            <Route path='/certificate-of-no-property-form' element={<CertificateOfNoProperty />} />
-            <Route path='/certificate-of-good-moral-form' element={<CertificateOfGoodMoral />} />
-            <Route path='/certificate-for-new-residents-form' element={<CertificateForNewResidents />} />
-            <Route path='/certificate-for-solo-parent-form' element={<CertificateForSoloParent />} />
+            <Route path='/certificate-of-residency-form' element={<CertificateOfResidency />}/>
+            <Route path='/certificate-of-indigency-form' element={<CertificateOfIndigency />}/>
+            <Route path='/certificate-of-low-income-form' element={<CertificateOfLowIncome />}/>
+            <Route path='/business-clearance-form' element={<BusinessClearance />}/>
+            <Route path='/certificate-of-death-form' element={<CertificateOfDeath />}/>
+            <Route path='/certificate-of-no-property-form' element={<CertificateOfNoProperty />}/>
+            <Route path='/certificate-of-good-moral-form' element={<CertificateOfGoodMoral />}/>
+            <Route path='/certificate-for-new-residents-form' element={<CertificateForNewResidents />}/>
+            <Route path='/certificate-for-solo-parent-form' element={<CertificateForSoloParent />}/>
+
+            <Route path='/pages/home' element={<Home />} />
+            <Route path='/pages/about' element={<About />} />
+            <Route path='/pages/services' element={<Services />} />
+            <Route path='/pages/track-request' element={<TrackRequest />} />
+            <Route path='/pages/faq' element={<Faq />} />
 
           </Routes>
           <Footer />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,11 @@
 import { HashLink } from 'react-router-hash-link';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ActiveSectionContext } from './ActiveSectionContext';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = ({ containerStyles }) => {
+  const [ActiveTab, setActiveTab] = useState();
+
   const { activeSection, setActiveSection } = useContext(ActiveSectionContext);
 
   const isActive = (hash) => activeSection === hash;
@@ -18,7 +21,7 @@ const Navbar = ({ containerStyles }) => {
 
   return (
     <nav className={containerStyles}>
-      {['home', 'about', 'services', 'track-request', 'location'].map((section) => (
+      {/* {['home', 'about', 'services', 'track-request', 'location'].map((section) => (
         <HashLink
           key={section}
           smooth
@@ -30,7 +33,49 @@ const Navbar = ({ containerStyles }) => {
             {section.toUpperCase().replace('-', ' ')}
           </div>
         </HashLink>
-      ))}
+
+      ))} */}
+
+      <NavLink to={'/pages/home'}>
+        {({isActive}) => (
+          <button className={`${isActive ? 'text-green-400 text-sm font-semibold flex-1 border-b-2 border-b-green-400' : 'hover:text-green-500'} p-2`}>
+            Home
+          </button>
+        )}
+      </NavLink>
+
+      <NavLink to={'/pages/about'}>
+        {({isActive}) => (
+          <button className={`${isActive ? 'text-green-400 text-sm font-semibold flex-1 border-b-2 border-b-green-400' : 'hover:text-green-500'} p-2`}>
+            About
+          </button>
+        )}
+      </NavLink>
+
+      <NavLink to={'/pages/services'}>
+        {({isActive}) => (
+          <button className={`${isActive ? 'text-green-400 text-sm font-semibold flex-1 border-b-2 border-b-green-400' : 'hover:text-green-500'} p-2`}>
+            Services
+          </button>
+        )}
+      </NavLink>
+
+      <NavLink to={'/pages-faq'}>
+        {({isActive}) => (
+          <button className={`${isActive ? 'text-green-400 text-sm font-semibold flex-1 border-b-2 border-b-green-400' : 'hover:text-green-500'} p-2`}>
+            FAQ
+          </button>
+        )}
+      </NavLink>
+
+      <NavLink to={'/pages/track-request'}>
+        {({isActive}) => (
+          <button className={`${isActive ? 'text-green-400 text-sm font-semibold flex-1 border-b-2 border-b-green-400' : 'hover:text-green-500'} p-2`}>
+            Track Request
+          </button>
+        )}
+      </NavLink>
+
     </nav>
   );
 };
